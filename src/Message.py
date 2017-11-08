4836731889ccfb34dca945254cec96cf8ab4feeb
yl54 <randyl@digitalchemy.us>
2017-11-08 12:05:07 -0800

    Redefine
    Redefined 'm_encry' to 'm_encrypt'

--- src/Message.py
+++ src/Message.py
@@ -1,5 +1,6 @@
-"""
+import Algorithms
 
+"""
 This module provides helper functions for RSA message transferring.
 
 """
@@ -11,4 +12,4 @@ def pickMessage():
 def encryptMessage(m):
 
 # Function to decrypt message.
-def decryptMessage(m_encry):
+def decryptMessage(m_encrypt):
