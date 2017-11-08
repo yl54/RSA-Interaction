d0fc17789dbd782cc0e399cb214c530ff21195ff
yl54 <randyl@digitalchemy.us>
2017-11-08 12:02:02 -0800

    Implement Setup features.
    Implemented picking initial primes and picking public exponent steps.

--- src/Setup.py
+++ src/Setup.py
@@ -1,34 +1,42 @@
-"""
+import Algorithms
 
+"""
 This module provides helper functions for RSA setup.
 
 """
 
 # Function to ask user for initial primes p and q.
 def pickInitialPrimes():
-    # do this in a loop.
+    picks = []
+    print "Please choose 2 initial prime numbers."
+    while(len(picks) < 2):
+        num = input("Enter an initial prime number: ")
+        isPrime = Algorithms.isPrime(num)
+        message = ""
+        if(isPrime):
+            picks.append(num)
+            message = str(num) + " is a prime number."
+        else:
+            message = str(num) + " is not a prime number."
+        print message
+    
+    return picks[0], picks[1]
 
-    # pick a number. 
-
-    # check if its a prime.
-
-    # return both values.
-
-# Function to ask user for the e value.
+# Function to ask user for the public exponent.
 def pickPublicExponent(phi):
     # do a while loop.
+    while(True):
+        num = input("Enter your public exponent: ")
+        isBtwn = (1 < num) & (num < phi)
+        isCoPrime = Algorithms.isCoPrime(e, phi)
+        if(isBtwn & is CoPrime):
+            print str(num) + " is a valid public exponent."
+            return num
+        else:
+            print str(num) + " is not a valid public exponent. Try again"
 
-    # pick a number.
-
-    # check if provided number is between 1 and phi.
-
-    # check if provided number is coprime with phi.
-
-    # return picked number.
-
-
-# Function to compute the d value.
-def computeSecretExponent(e, phi):
+# Function to calculate the secret exponent.
+def calcSecretExponent(e, phi):
     # compute: e * d congr 1 (mod phi)
 
     # return result.
