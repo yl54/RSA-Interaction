cabc4666f3d5b0e6796e0b59255eca840ffe7923
yl54 <randyl@digitalchemy.us>
2017-11-09 11:00:12 -0800

    Add to Algorithms.py skeleton
    Added skeleton for calculating the euclidean algorithm, the extended euclidean algorithm, and division.

--- src/Algorithms.py
+++ src/Algorithms.py
@@ -26,7 +26,38 @@ def isCoPrime(val_1, val_2):
     
     # return if the gcd is equal to 1 or not.
 
+# Function to execute the euclidean algorithm on two numbers.
+def calcEuclid(val_1, val_2):
+    # do while loop until gcd remainder is 0
+        # identify which number is smaller.
+
+        # use division algorithm.
+
+        # change variables around.
+
+        # store each of these iterations.
+
+    # return the g.c.d and iterations.
+
+
+# Function to execute the extended euclidean algorithm.
+def calcExtendEclid(result, iters):
+    # do while loop until iterations get to beginning
+        # get the next number in the iteration.
+
+        # find the number that fits the amount.
+
+        # change variables around
+
+    # return the multipliers 
+
+# Function to execute division algorithm and get the quotient and remainder.
+def calcDivision(dividend, divisor):
+    # get quotient and remainder
+
 
 # Probably wise to have an object that stores previous results of 
 #   algorithm if its gonna be a big number.
 # First, do the way without storage.
+
+
