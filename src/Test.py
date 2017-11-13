87ab130a801fd490fe54e5e84ce94422dcdfe28b
yl54 <randyl@digitalchemy.us>
2017-11-13 15:16:10 -0800

    Added Test.py.
    Added a test program that tests different functions from the modules.

---
+++ src/Test.py
@@ -0,0 +1,23 @@
+import Algorithms
+import Message
+import Setup
+
+"""
+This module does tests on some of the functions to see
+  if they are working properly or not. 
+
+"""
+
+# Test the prime function.
+
+# Test the coprime function.
+
+# Test the Euclidean Algorithm.
+
+# Test the Extended Euclidean Algorithm.
+
+# Test the successive squaring mod algorithm.
+
+# Test the encryption of a message.
+
+# Test the decryption of a message.
